786af78db5922d00686e95bbc41985ae2536d979
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-04-02 16:35:43 -0400

    added help

--- server.express.js
+++ server.express.js
@@ -1,76 +1,73 @@
-(function(express, all, open, request, writeFile, exec, Parse, Readable, defer, createWriteStream){
+(function(express, all, spawn, request, open, mkdir, writeFile, readdir, join, defer){
     "use strict";
-    var unzip = function(zip){
+    var statics = function(){
 	var deferred = defer();
-	/*
-	var s = new Readable();
-	s.push(zip);
-	s.push(null);
-	s.pipe(Parse()).on("entry", function(entry, error){
-	    if(error){
-		return deferred.reject(error);
-	    }else{
-		return deferred.resolve(entry);
-	    }
-	});
-	*/
-	deferred.resolve({
-	    pipe: function(){
-	    }
-	});
-	return deferred.promise;
-    };
-    var pipe = function(entry,path){
-	var deferred = defer();
-	entry.pipe(createWriteStream(path));
-	deferred.resolve(path);
+	deferred.resolve([
+	    "/usr/share/jenkins/webroot/WEB-INF/jenkins-cli.jar",
+	    "/usr/share/jenkins/webroot/WEB-INF/remoting.jar",
+	    "/usr/share/jenkins/webroot/WEB-INF/slave.jar",
+	    "/usr/share/jenkins/webroot/WEB-INF/classes:localizer-1.9.jar"
+	]);
 	return deferred.promise;
     };
     return all([
-	request("http://www.java2s.com/Code/JarDownload/localizer/localizer-1.9.jar.zip").then(function(localizer){
-	    return unzip(localizer).then(function(entry){
-		return open("temp").then(function(temp){
-		    return pipe(entry,temp.path);
+	open("").then(function(zipFile){
+	    return spawn("curl", ["--output", zipFile.path, "http://www.java2s.com/Code/JarDownload/localizer/localizer-1.9.jar.zip"]).then(function(){
+		return mkdir("").then(function(directory){
+		    return spawn("unzip", ["-d", directory, zipFile.path]).then(function(){
+			return readdir(directory).map(function(file){
+			    return join(directory,file);
+			});
+		    });
 		});
 	    });
 	}),
 	request("http://central.maven.org/maven2/commons-codec/commons-codec/1.9/commons-codec-1.9.jar").then(function(codec){
 	    return open("temp").then(function(temp){
-		return writeFile(codec,temp.path).then(function(){
-		    return temp.path;
+		return writeFile(temp.path,codec).then(function(){
+		    return [temp.path];
 		});
 	    });
-	})
+	}),
+	statics()
     ]).then(function(fileNames){
-	return fileNames.map(function(fileName){
-	    return console.log(fileName);
-	});
-	/*
-	  return express()
-	  .get("/rest/api/0/status/health", function(request, response){
-	  return response.json({
-		    status: "OK"
-		    });
-		    })
+	return [].concat.apply([], fileNames).join(":");
+    }).then(function(fileNames){
+	return express()
+	    .get("/rest/api/0/status/health", function(request, response){
+		return response.json({
+		    status: true
+		});
+	    })
 	    .get("/rest/api/0/jenkins/help", function(request, response){
-	    return exec().then(function(success){
-	    }, function(rageguy){
-	    });
+		return spawn("java", ["-classpath", fileNames, "hudson.cli.CLI", "help"]).then(function(success){
+		    return response.json({
+			status: true,
+			out: success.stdout,
+			err: success.stderr
+		    });
+		}, function(rageguy){
+		    return response.json({
+			status: false,
+			error: JSON.stringify(rageguy)
+		    });
+		});
 	    })
 	    .listen(28494)
-	    ;
-	*/
+	;
     }, function(rageguy){
+	console.log("PROBLEM");
+	console.log(rageguy);
     });
 }(
     require("express"),
     require("q").all,
-    require("promise-temp").open,
+    require("child-process-promise").spawn,
     require("request-promise"),
+    require("promise-temp").open,
+    require("promise-temp").mkdir,
     require("fs-promise").writeFile,
-    require("child-process-promise").exec,
-    require("unzip").Parse,
-    require("stream").Readable,
-    require("q").defer,
-    require("fs-promise").createWriteStream
+    require("fs-promise").readdir,
+    require("path").join,
+    require("q").defer
 ));
