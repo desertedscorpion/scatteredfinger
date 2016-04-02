2f669d4ca80693bfa65fa16926457871961c468d
Emory Merryman <emory.merryman+HLmI8wR4utSYuirp@gmail.com>
2016-04-02 18:08:36 +0000

    downloadingfiles

--- server.express.js
+++ server.express.js
@@ -1,6 +1,22 @@
-(function(express, mkdir, creaexec){
+(function(express, all, open, request, writeFile, exec){
     "use strict";
-    return mkdir().then(function(success){
+    return all([
+	request("http://www.java2s.com/Code/JarDownload/localizer/localizer-1.9.jar.zip"),
+	request("http://central.maven.org/maven2/commons-codec/commons-codec/1.9/commons-codec-1.9.jar")
+    ]).then(function(files){
+	return all(files.map(function(file){
+	    return open("temp").then(function(temp){
+		return writeFile(temp.path,file).then(function(success){
+		    console.log(temp.path)
+		    return temp.path;
+		});
+	    });
+	}));
+    }).then(function(fileNames){
+	return fileNames.map(function(fileName){
+	    return console.log(fileName);
+	});
+	/*
 	return express()
 	    .get("/rest/api/0/status/health", function(request, response){
 		return response.json({
@@ -14,10 +30,14 @@
 	    })
 	    .listen(28494)
 	;
+	*/
     }, function(rageguy){
     });
 }(
     require("express"),
-    require("promise-temp").mkdir,
+    require("q").all,
+    require("promise-temp").open,
+    require("request-promise"),
+    require("fs-promise").writeFile,
     require("child-process-promise").exec)
 );
